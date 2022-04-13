6c4dd3f045d8f0c2059284b255c1299a598a1b42
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2022-04-13 22:36:22 +0530

    Create 59-spiral-matrix-ii.cpp

---
+++ LeetCode_April_Challenge/59-spiral-matrix-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n));
+        int sR=0,sC=0;
+        int eR=n-1,eC=n-1;
+        int dir=0;
+        int cnt=1;
+        
+        while(sR<=eR && sC<=eC){
+            if(dir ==0){
+                for(int i=sC;i<=eC;i++)
+                    res[sR][i]=cnt++;
+                sR++;
+            }else if(dir ==1){
+                for(int i=sR;i<=eR;i++)
+                    res[i][eC]=cnt++;
+                eC--;
+            }else if(dir ==2){
+                for(int i=eC;i>=sC;i--)
+                    res[eR][i]=cnt++;
+                eR--;
+            }else if(dir ==3){
+                for(int i=eR;i>=sR;i--)
+                    res[i][sC]=cnt++;
+                sC++;
+            }
+            dir = (dir+1)%4;
+        }
+        return res;
+    }
+};
