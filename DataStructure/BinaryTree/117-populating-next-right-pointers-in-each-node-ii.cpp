8ec1518152044f475ab4f73ac5db85392fa694cb
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2022-04-15 12:04:36 +0530

    Leetcode 117: Populatinf next right ptr

---
+++ DataStructure/BinaryTree/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+           if(root == nullptr)
+            return nullptr;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            Node* nextNode = nullptr;
+            for(int i=q.size();i>0;i--){
+                Node *tmp = q.front();
+                q.pop();
+                tmp->next = nextNode;
+                nextNode = tmp;
+                if(tmp->right)
+                    q.push(tmp->right);
+                if(tmp->left)
+                    q.push(tmp->left);
+            }
+        }
+        return root;
+    }
+};
