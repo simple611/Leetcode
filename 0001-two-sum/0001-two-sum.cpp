c5de954767030b319d74d7eb461a2e4c2689ed42
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-12 20:47:29 +0530

    Time: 8 ms (94.11%), Space: 10.6 MB (51.29%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /*// O(n^2) | O(1) space
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+                // else if(nums[i]+ nums[j] > target)
+                //     break;
+            }
+        }
+        return res;
+        
+        */
+        
+        // O(nlogn) | O(1) space
+        
+        
+        vector<pair<int,int>> v(nums.size());
+        for(int i=0;i<nums.size();i++){
+            v[i].first=nums[i];
+            v[i].second=i;
+        }
+        int n=nums.size(),i=0,j=n-1;
+        sort(v.begin(),v.end());
+       
+        // vector<int> ans;
+        while(i<j){
+            if(v[i].first+v[j].first==target){
+                return {v[i].second,v[j].second};
+            }
+            else if(v[i].first+v[j].first>target)j--;
+            else i++;
+        }
+        return {};
+        
+        
+    }
+};
