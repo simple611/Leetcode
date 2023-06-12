047e3ab0cca7c5e9fcd07b7922e8d622e8ad6e70
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-12 21:16:54 +0530

    Time: 18 ms (54.97%), Space: 11.1 MB (14.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -24,24 +24,35 @@ public:
         // O(nlogn) | O(1) space
         
         
-        vector<pair<int,int>> v(nums.size());
-        for(int i=0;i<nums.size();i++){
-            v[i].first=nums[i];
-            v[i].second=i;
-        }
-        int n=nums.size(),i=0,j=n-1;
-        sort(v.begin(),v.end());
+//         vector<pair<int,int>> v(nums.size());
+//         for(int i=0;i<nums.size();i++){
+//             v[i].first=nums[i];
+//             v[i].second=i;
+//         }
+//         int n=nums.size(),i=0,j=n-1;
+//         sort(v.begin(),v.end());
        
-        // vector<int> ans;
-        while(i<j){
-            if(v[i].first+v[j].first==target){
-                return {v[i].second,v[j].second};
+//         // vector<int> ans;
+//         while(i<j){
+//             if(v[i].first+v[j].first==target){
+//                 return {v[i].second,v[j].second};
+//             }
+//             else if(v[i].first+v[j].first>target)j--;
+//             else i++;
+//         }
+//         return {};
+        
+        //O(n) | O(n)
+        map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            int val = target - nums[i];
+            
+            if(mp.find(val) != mp.end()){
+                return {i, mp[val]};
             }
-            else if(v[i].first+v[j].first>target)j--;
-            else i++;
+            mp[nums[i]] = i;
         }
+        
         return {};
-        
-        
     }
 };
