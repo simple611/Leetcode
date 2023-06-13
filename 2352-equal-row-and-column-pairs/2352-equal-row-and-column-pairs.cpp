25123ea029da2ef8bbe09786ef0736e2ba9df486
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-13 22:36:30 +0530

    Time: 115 ms (57.07%), Space: 34.9 MB (38.89%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,24 +2,46 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         
-        // TC - O(n^3)  SC - O(1)
-        int count = 0, n = grid.size();
-        for(int r=0;r<n;r++){
-            for(int c=0;c<n;c++){
-                bool match = true;
+//         // TC - O(n^3)  SC - O(1)
+//         int count = 0, n = grid.size();
+//         for(int r=0;r<n;r++){
+//             for(int c=0;c<n;c++){
+//                 bool match = true;
                 
                 
-                for(int i=0;i<n;i++){
-                    if(grid[r][i] != grid[i][c]){
-                        match = false;
-                        break;
-                    }
-                }
+//                 for(int i=0;i<n;i++){
+//                     if(grid[r][i] != grid[i][c]){
+//                         match = false;
+//                         break;
+//                     }
+//                 }
                 
-                count += match ?1 : 0;
-            }
+//                 count += match ?1 : 0;
+//             }
+//         }
+//         return count;
+        
+        
+        // Hash Map approach
+        // TC - O(n^2) SC - O(n^2)
+        
+        int count = 0;
+        int n = grid.size();
+        
+        map<vector<int>, int> mp;
+        
+        for(vector<int> row: grid){
+            mp[row]++;
         }
         
+        for(int j=0;j<grid[0].size();j++){
+            
+            vector<int> val;
+            for(int k=0;k<n;k++){
+                val.push_back(grid[k][j]);
+            }
+            count += mp[val];
+        }
         return count;
     }
 };
