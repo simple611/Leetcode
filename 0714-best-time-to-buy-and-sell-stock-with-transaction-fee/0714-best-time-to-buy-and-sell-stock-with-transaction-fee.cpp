8df43967c1818c8ae018c87ef7c5e93bf51a0c34
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-22 23:32:11 +0530

    Time: 100 ms (97.48%), Space: 54.8 MB (98.47%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<int> free(n, 0), hold(n, 0);
         
-        // In order to hold a stock on day 0, 
-        //we have no other choice but to buy it for prices[0].
-        hold[0] = -prices[0];
+//         //TC - O(n)   SC- O(n)
+//         int n = prices.size();
+//         vector<int> free(n, 0), hold(n, 0);
+        
+//         // In order to hold a stock on day 0, 
+//         //we have no other choice but to buy it for prices[0].
+//         hold[0] = -prices[0];
+        
+//         for (int i = 1; i < n; i++) {
+//             free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee);
+//             hold[i] = max(hold[i - 1], free[i - 1] - prices[i]);
+//         }
+        
+//         return free[n - 1];
+        
+        int n = prices.size();
+        int free = 0, hold = -prices[0];
         
         for (int i = 1; i < n; i++) {
-            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee);
-            hold[i] = max(hold[i - 1], free[i - 1] - prices[i]);
+            int tmp = hold;
+            hold = max(hold, free - prices[i]);
+            free = max(free, tmp + prices[i] - fee);
         }
         
-        return free[n - 1];
+        return free;
     }
 };
