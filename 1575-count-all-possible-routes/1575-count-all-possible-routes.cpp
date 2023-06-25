891b0c3c4b740c9bcd4e2a09c7b2198f9a106fe5
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-26 01:39:28 +0530

    Time: 172 ms (89.35%), Space: 11.9 MB (44.42%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(vector<int>& loc, int curCity, int finish, int remainingFuel,
+              vector<vector<int>>& memo) {
+        if (remainingFuel < 0) {
+            return 0;
+        }
+        if (memo[curCity][remainingFuel] != -1) {
+            return memo[curCity][remainingFuel];
+        }
+
+        int ans = curCity == finish ? 1 : 0;
+        for (int nextCity = 0; nextCity < loc.size(); nextCity++) {
+            if (nextCity != curCity) {
+                ans = (ans + solve(loc, nextCity, finish,
+                                   remainingFuel - abs(loc[curCity] - loc[nextCity]),
+                                   memo)) % 1000000007;
+            }
+        }
+
+        return memo[curCity][remainingFuel] = ans;
+    }
+
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        int n = locations.size();
+        vector<vector<int>> memo(n, vector<int>(fuel + 1, -1));
+
+        return solve(locations, start, finish, fuel, memo);
+    }
+};
