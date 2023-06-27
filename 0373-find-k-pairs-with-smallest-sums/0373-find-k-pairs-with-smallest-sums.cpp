82372afd31d5960352fbb7bc126d4a64357da29a
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-06-27 17:16:49 +0530

    Time: 355 ms (52.08%), Space: 108.8 MB (87.95%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         priority_queue<pair<int,pair<int,int>>> pq;  // default: the greatest comes top
@@ -23,5 +23,38 @@ public:
         }
         reverse(ans.begin(),ans.end());
         return ans;
+    }
+};
+
+class Solution {
+private:
+    struct mycompare{
+        bool operator()(pair<int, int>& p1, pair<int, int>& p2){
+            return p1.first + p1.second < p2.first + p2.second;
+        }
+    };
+public:
+    //nums1: m nums2: n
+ // TC: O(m * n logk)    SC- O(k)
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> res;
+        priority_queue<pair<int,int>, vector<pair<int, int> >, mycompare> pq;
+        for(int i = 0; i < min((int)nums1.size(), k); i++){
+            for(int j = 0; j < min((int)nums2.size(), k); j++){
+                if(pq.size() < k)
+                    pq.push(make_pair(nums1[i], nums2[j]));
+                else if(nums1[i] + nums2[j] < pq.top().first + pq.top().second){
+                    pq.push(make_pair(nums1[i], nums2[j]));
+                    pq.pop();
+                } else {
+                    break;  // save time! since we don't need to traverse the rest of vector 2
+                }
+            }
+        }
+        while(!pq.empty()){
+            res.push_back({pq.top().first, pq.top().second});
+            pq.pop();
+        }
+        return res;
     }
 };
