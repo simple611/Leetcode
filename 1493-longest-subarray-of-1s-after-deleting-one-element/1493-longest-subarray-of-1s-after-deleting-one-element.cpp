0b4c8c16602ba3e245787874119bd718bf85a47c
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-07-05 23:39:53 +0530

    Time: 44 ms (67.28%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
@@ -21,4 +21,24 @@ public:
         }
         return (ans == n) ? ans - 1 : ans;
     }
+};
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int prev = 0, curr = 0;
+        int ans = 0;
+        for (int i : nums) {
+            if (i == 1)
+                curr++;
+            else {
+                ans = std::max(ans, curr + prev);
+                prev = curr;
+                curr = 0;
+            }
+        }
+        ans = std::max(ans, curr + prev);
+
+        return (ans == nums.size()) ? ans - 1 : ans;
+            }
 };
