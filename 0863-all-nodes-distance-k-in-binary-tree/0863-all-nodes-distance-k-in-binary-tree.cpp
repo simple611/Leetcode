b98a4ae5fc6f4acc8afaa7d5525d04a969c4ac7a
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-07-11 23:37:36 +0530

    Time: 9 ms (38.57%), Space: 13 MB (75.09%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
+    unordered_map<TreeNode*, bool> visited;
+    vector<int> result;
+public:
+
+    void findParent(TreeNode * root){
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty()) { /*First BFS to get a track of parent nodes*/
+            TreeNode* current = queue.front(); queue.pop();
+            if(current->left) {
+                parent_track[current->left] = current;
+                queue.push(current->left);
+            }
+            if(current->right) {
+                parent_track[current->right] = current;
+                queue.push(current->right);
+            }
+        }
+    }
+    
+    void bfs(TreeNode* target, int K){
+        queue<TreeNode*> queue; 
+        queue.push(target);
+        visited[target] = true;
+        int curr_level = 0;
+        while(!queue.empty()) { /*Second BFS to go upto K level from target node and using our hashtable info*/
+            int size = queue.size();
+            if(curr_level++ == K) break;
+            for(int i=0; i<size; i++) {
+                TreeNode* current = queue.front(); queue.pop();
+                if(current->left && !visited[current->left]) {
+                    queue.push(current->left);
+                    visited[current->left] = true;
+                }
+                if(current->right && !visited[current->right]) {
+                    queue.push(current->right);
+                    visited[current->right] = true;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]) {
+                    queue.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+        while(!queue.empty()) {
+            TreeNode* current = queue.front(); queue.pop();
+            result.push_back(current->val);
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        findParent(root);
+        bfs(target, k);
+        return result;
+    }
+};
