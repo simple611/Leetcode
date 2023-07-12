b63204e753f731205e2f0ceb661e0a38710ed99e
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-07-12 22:52:24 +0530

    Time: 216 ms (53.76%), Space: 60.8 MB (70.00%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        
+        // TC - O(nlogn)  SC - O(1)
+//         int n = nums.size();
+//         if( n==0) return 0;
+        
+//         sort(nums.begin(), nums.end());
+        
+//         int longestS=0;
+//         int curC=1;
+//         for(int i=1;i<n;i++){
+//             if(nums[i] != nums[i-1]){
+//                 if(nums[i] == nums[i-1]+1){
+//                     curC+=1;
+//                 } else {
+//                     longestS = max(longestS, curC);
+//                     curC = 1;
+//                 }
+//             }
+//         }
+//         return max(longestS, curC);
+        
+        
+        
+        // TC - O(n)  SC - O(n)
+        int n = nums.size();
+        if(n==0) return 0;
+        
+        unordered_set<int> uSet(begin(nums), end(nums)); 
+	    int longestS = 0;
+	    for(auto& num : uSet) {
+		    int curC = 1;
+		// find consecutive elements in the backward and forward direction from num
+		    for(int j = 1; uSet.count(num - j); j++) uSet.erase(num - j), curC++;
+		    for(int j = 1; uSet.count(num + j); j++) uSet.erase(num + j), curC++;
+		    longestS = max(longestS, curC);  // update longest to hold longest consecutive sequence till now
+	    }
+	    return longestS;
+        
+    }
+};
