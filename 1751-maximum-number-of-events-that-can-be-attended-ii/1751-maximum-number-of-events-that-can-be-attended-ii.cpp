5ac09ebd8074017e2673e06968d0d388322d707f
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-07-15 16:46:54 +0530

    Time: 403 ms (16.59%), Space: 113.3 MB (6.00%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -8,14 +8,23 @@ public:
         
         if(dp[pos][k] != -1)
             return dp[pos][k];
-        int i;
-        for(i=pos+1;i<n;i++){
-            if(events[i][0] > events[pos][1])
-                break;
-        }
+        
+        //Linear Search for next index
+        // int i;
+        // for(i=pos+1;i<n;i++){
+        //     if(events[i][0] > events[pos][1])
+        //         break;
+        // }
+        
+        // Binary search for next index
+        vector<int> temp = {events[pos][1], INT_MAX, INT_MAX};
+        int i=upper_bound(events.begin()+1,events.end(),temp)-events.begin();
+        
         return dp[pos][k] = max(solve(events, n, pos+1, k, dp), events[pos][2] + solve(events, n, i, k-1, dp));
     }
     
+    
+    // TC - O((n*k)*n)  SC - O(n*k)
     int maxValue(vector<vector<int>>& events, int k) {
         
         sort(events.begin(), events.end());
