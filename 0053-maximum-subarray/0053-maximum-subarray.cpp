5b3801bed36b3be013b0cf43fd88bde5ee5d03b5
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-01 20:07:34 +0530

    Time: 69 ms (99.80%), Space: 67.7 MB (32.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     //TC - O(n)   SC- O(1)
     int maxSubArray(vector<int>& nums) {
-//         int curMax = 0;
-//         int maxSum = INT_MIN;
+        int curMax = 0;
+        int maxSum = INT_MIN;
         
-//         for(int i=0;i<nums.size();++i)
-//         {
-//             curMax += nums[i];
-//             if(curMax > maxSum)
-//                 maxSum = curMax;
+        for(int i=0;i<nums.size();++i)
+        {
+            curMax += nums[i];
+            if(curMax > maxSum)
+                maxSum = curMax;
             
-//             if(curMax < 0)
-//                 curMax = 0;
-//         }
-//         return maxSum;
+            if(curMax < 0)
+                curMax = 0;
+        }
+        return maxSum;
         
-        int curMax = 0, maxTillNow = INT_MIN;
-        for(auto c : nums)
-            curMax = max(c, curMax + c),
-            maxTillNow = max(maxTillNow, curMax);
-        return maxTillNow;
+        // int curMax = 0, maxTillNow = INT_MIN;
+        // for(auto c : nums)
+        //     curMax = max(c, curMax + c),
+        //     maxTillNow = max(maxTillNow, curMax);
+        // return maxTillNow;
     }
 };
