6af224fede9d44a354ed6b7b0275670f24d8dfe3
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-16 23:54:12 +0530

    Time: 26 ms (53.37%), Space: 25.2 MB (15.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -29,26 +29,38 @@ public:
     }    
     
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n), right(n), ans(n);
+//         int n = nums.size();
+//         vector<int> left(n), right(n), ans(n);
         
-        left[0] = nums[0];
-        for(int i=1;i<n;i++){
-            left[i] = nums[i] * left[i-1];
+//         left[0] = nums[0];
+//         for(int i=1;i<n;i++){
+//             left[i] = nums[i] * left[i-1];
+//         }
+        
+//         right[n-1] = nums[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             right[i] = nums[i] * right[i+1];
+//         }
+        
+//         ans[0] = right[1];
+//         ans[n-1] = left[n-2];
+        
+//         for(int i=1;i<n-1;i++){
+//             ans[i] = left[i-1]*right[i+1];    
+//         }
+        
+//         return ans;
+        vector<int> lhs(nums.size(), 1);
+        vector<int> rhs(nums.size(), 1);
+        for(int i = 1; i < nums.size();i++){
+            lhs[i] = lhs[i-1] * nums[i-1];
         }
-        
-        right[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            right[i] = nums[i] * right[i+1];
+        for(int i = nums.size()-1 ; i >= 1 ; i--){
+            rhs[i-1] = rhs[i] * nums[i];
         }
-        
-        ans[0] = right[1];
-        ans[n-1] = left[n-2];
-        
-        for(int i=1;i<n-1;i++){
-            ans[i] = left[i-1]*right[i+1];    
+        for(int i = 0;i<nums.size() ;i++){
+            nums[i] = lhs[i] * rhs[i];
         }
-        
-        return ans;
+        return nums;
     }
 };
