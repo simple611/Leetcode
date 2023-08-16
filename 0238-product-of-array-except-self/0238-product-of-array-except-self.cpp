c284ca778802d275e7474c3fc09ce327dd6b4dc9
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-16 23:53:17 +0530

    Time: 16 ms (90.83%), Space: 25.2 MB (15.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    
+    // TC - O(n)  SC- O(1)
+    vector<int> productExceptSelf_withDiv(vector<int>& nums) {
         
         int product = 1; int zeroCnt = count(nums.begin(), nums.end(), 0);
         
         if(zeroCnt > 1) return vector<int>(nums.size(), 0);
-        
-        
+             
         for(int a: nums){
             if(a) {
                 product = product*a;
@@ -23,8 +24,31 @@ public:
             
             res.push_back(tmp);
         }
-        
-        
+          
         return res;
+    }    
+    
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n), right(n), ans(n);
+        
+        left[0] = nums[0];
+        for(int i=1;i<n;i++){
+            left[i] = nums[i] * left[i-1];
+        }
+        
+        right[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = nums[i] * right[i+1];
+        }
+        
+        ans[0] = right[1];
+        ans[n-1] = left[n-2];
+        
+        for(int i=1;i<n-1;i++){
+            ans[i] = left[i-1]*right[i+1];    
+        }
+        
+        return ans;
     }
 };
