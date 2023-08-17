5b2404787b3c48b5df52348c04d93f5c3329b832
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-18 00:23:34 +0530

    Time: 54 ms (79.78%), Space: 41.1 MB (92.42%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -25,24 +25,52 @@ public:
         
         return count;
     }
-    int firstMissingPositive(vector<int>& nums) {
+    
+    // TC - O(n)  SC- O(1)
+    int firstMissingPositive_withoutSwap(vector<int>& nums) {
         int n = nums.size();
         
+        // Set all negative and greater than n numbers to n+1, since don't need to consider them
         for(int i=0;i<n;i++){
             if(nums[i]<=0 || nums[i] > n)
                 nums[i] = n+1;
         }
         
+        // Mark all elements positive as negative if they exists
         for(int i=0;i<n;i++){
             if(abs(nums[i]) >=n+1) continue;
             int pos = abs(nums[i]);
             nums[pos-1] = -1 * abs(nums[pos-1]);
         }
         
+        // Check for positive number and return its indx
         for(int i =0;i<n;i++){
             if(nums[i]>0) return i+1;
         }
         
+        return n+1;
+    }
+    
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            int ele=nums[i];
+            
+            if(ele>=1 && ele<=n){
+                int pos=ele-1;
+                
+                if(nums[pos]!=ele){
+                    swap(nums[pos],nums[i]);
+                    i--;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
         return n+1;
     }
 };
