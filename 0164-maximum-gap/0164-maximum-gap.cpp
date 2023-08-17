6ceaa818ad50438b22ab4402b024aba88c98346c
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-17 23:03:07 +0530

    Time: 135 ms (95.11%), Space: 89.1 MB (27.46%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int maximumGap(vector<int>& nums) {
+    
+    // TC- O(nlogn) + O(n)   SC- O(1)
+    int maximumGap__sorting(vector<int>& nums) {
         if(nums.size() < 2)
             return 0;
         sort(nums.begin(), nums.end());
@@ -13,5 +15,39 @@ public:
         }
         
         return maxDiff;
+    }
+    
+    int maximumGap(vector<int>& nums) {
+        if(nums.size() < 2)
+            return 0;
+        int minN = INT_MAX, maxN=INT_MIN;
+        
+        for(int i=0;i<nums.size();i++){
+            minN = min(nums[i], minN);
+            maxN = max(nums[i], maxN);
+        }
+        
+        double gap = ((maxN - minN)*1.0)/(nums.size() -1);
+        if(gap < 1.0 ) gap =1;
+        
+        vector<int> minBucket(nums.size(), INT_MAX);
+        vector<int> maxBucket(nums.size(), INT_MIN);
+        
+        for(int i=0;i<nums.size();i++){
+            int pos = (nums[i] - minN )/gap;
+            
+            minBucket[pos] = min(minBucket[pos], nums[i]);
+            maxBucket[pos] = max(maxBucket[pos], nums[i]);
+        }
+        
+        int prevMax = maxBucket[0];
+        int ans = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(minBucket[i] == INT_MAX) continue;
+            ans = max(ans, minBucket[i] - prevMax);
+            prevMax = maxBucket[i];
+        }
+        return ans;
     }
 };
