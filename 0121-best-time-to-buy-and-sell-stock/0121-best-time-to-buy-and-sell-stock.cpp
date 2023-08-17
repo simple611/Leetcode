31650f283cd3b81bad3f7f3aaa94100ba70f6762
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-18 00:38:51 +0530

    Time: 108 ms (70.54%), Space: 93.4 MB (34.18%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+//         int maxP=0;
+//         for(int i=0;i<prices.size();++i)
+//         {
+//             for(int j=i+1;j<prices.size();++j)
+//             {
+//                 if(prices[j] - prices[i] > maxP)
+//                     maxP = prices[j]-prices[i];
+//             }
+            
+//         }
+//         return maxP;
+        
+        
+        // int n = prices.size();
+//         if(prices.size() == 0)
+//             return 0;
+//         int maxP = 0;
+//         int minP = INT_MAX;
+        
+//         for(int i=0;i<prices.size();++i)
+//         {
+//             minP = min(minP, prices[i]);
+//             maxP = max(maxP, prices[i] - minP);
+//         }
+//         return maxP;
+        
+        int n = prices.size();
+        int buyAtPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i=0; i<n; i++){
+            buyAtPrice = min(buyAtPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - buyAtPrice); 
+        }
+        return maxProfit;
+    }
+};
