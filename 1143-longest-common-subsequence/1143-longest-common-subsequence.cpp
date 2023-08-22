51c8cfc56e302870387c7a5973be0aa26eaa482b
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-22 22:47:32 +0530

    Time: 60 ms (53.80%), Space: 28.8 MB (12.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // TC - O(2^ m+n)   SC- O(m+n)
     int LCS_recursive(string text1, string text2, int m, int n){
         if(m == 0 || n==0)
             return 0;
@@ -14,6 +15,7 @@ public:
     }
     
     int dp[1001][1001];
+    // TC - O(m*n)   Sc- O(m*n)
     int LCS_memoize(string &text1, string &text2, int m, int n){
         if(m == 0 || n==0)
             return 0;
@@ -27,10 +29,35 @@ public:
             return dp[m][n] = max(LCS_memoize(text1, text2, m-1, n), LCS_memoize(text1, text2, m, n-1));
         }
     }
+    
+    int LCS_bottomUp(string &text1, string &text2, int m, int n){
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0)); 
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+          
+                if(text1[i-1] == text2[j-1]){
+                 
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+             
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    
     int longestCommonSubsequence(string text1, string text2) {
         // return LCS_recursive(text1, text2, text1.size(), text2.size());
         
-        memset(dp, -1, sizeof(dp));
-        return LCS_memoize(text1, text2, text1.size(), text2.size());
+        // memset(dp, -1, sizeof(dp));
+        // return LCS_memoize(text1, text2, text1.size(), text2.size());
+        
+        
+        return LCS_bottomUp(text1, text2, text1.size(), text2.size());
     }
 };
