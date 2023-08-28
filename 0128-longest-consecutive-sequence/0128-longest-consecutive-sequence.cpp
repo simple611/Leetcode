1afe8f8a4c2bdae10054289ff0df517f9b44cf75
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-08-28 23:03:17 +0530

    Time: 65 ms (95.18%), Space: 43.5 MB (91.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -24,21 +24,43 @@ public:
 //         return max(longestS, curC);
         
         
-        
-        // TC - O(n)  SC - O(n)
         int n = nums.size();
         if(n==0) return 0;
         
-        unordered_set<int> uSet(begin(nums), end(nums)); 
-	    int longestS = 0;
-	    for(auto& num : uSet) {
-		    int curC = 1;
-		// find consecutive elements in the backward and forward direction from num
-		    for(int j = 1; uSet.count(num - j); j++) uSet.erase(num - j), curC++;
-		    for(int j = 1; uSet.count(num + j); j++) uSet.erase(num + j), curC++;
-		    longestS = max(longestS, curC);  // update longest to hold longest consecutive sequence till now
-	    }
-	    return longestS;
+        sort(nums.begin(), nums.end());
+        
+        int prev = nums[0];
+        int count = 1;
+        int largest = 1;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i] == nums[i-1]+1){
+                count++;
+                prev = nums[i];
+            } else if( nums[i] != prev){
+                count = 1;
+                prev = nums[i];
+            }    
+            largest = max(largest, count);
+        }
+        return largest;
+        
+        
+        
+//         // TC - O(n)  SC - O(n)
+//         int n = nums.size();
+//         if(n==0) return 0;
+        
+//         unordered_set<int> uSet(begin(nums), end(nums)); 
+// 	    int longestS = 0;
+// 	    for(auto& num : uSet) {
+// 		    int curC = 1;
+// 		// find consecutive elements in the backward and forward direction from num
+// 		    for(int j = 1; uSet.count(num - j); j++) uSet.erase(num - j), curC++;
+// 		    for(int j = 1; uSet.count(num + j); j++) uSet.erase(num + j), curC++;
+// 		    longestS = max(longestS, curC);  // update longest to hold longest consecutive sequence till now
+// 	    }
+// 	    return longestS;
         
     }
 };
