6a411dd498172a61f4cd174559922ca2778b5d57
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-09-06 20:48:57 +0530

    Time: 28 ms (62.97%), Space: 89.3 MB (59.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -42,7 +42,7 @@ public:
     }
     
     // TC - O(m+n)  SC - O(1)
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    double findMedianSortedArrays_mergeSort_without_extraSpace(vector<int>& nums1, vector<int>& nums2) {
         
         int m = nums1.size();
         int n = nums2.size();
@@ -93,4 +93,45 @@ public:
         return (double)((double)idx1el + idx2el)/2.0;
         
     }
-};
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        if(n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int low =  0, high = n1;
+        
+        int leftPartition = (n1 + n2 + 1)/2;   // If total is odd 5+1/2 = 3, If even 3+3+1/2 = 3
+        
+        while( low <= high){
+            int mid1 = low + (high-low)/2;
+            
+            int mid2 = leftPartition - mid1;
+            
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+            
+            if(l1 <= r2 && l2 <= r1){  // Found a valid partition
+                if( (n1+n2) %2 == 1){
+                    return max(l1, l2);
+                } else {
+                    return ((double)(max(l1, l2) + min(r1, r2)))/2.0;
+                }
+            } else if( l1 > r2)
+                high = mid1-1;
+            else
+                low = mid1+1;
+                 
+            
+        }
+        
+        return 0;
+    }
+    };
