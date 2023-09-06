4da4d3988eae33d1d6924d36a889e125f720e1b9
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-09-06 16:11:22 +0530

    Time: 416 ms (11.56%), Space: 129 MB (49.13%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -0,0 +1,131 @@
+class TimeMap_MemoryExceeded {
+public:
+
+    
+    // map of key value
+    unordered_map<string, string> mp;
+    
+    //map of key -> timestamp
+    unordered_map<string, vector<int>> tmp;
+    
+    
+    int getTimeStamp(int time, vector<int>& arr){
+        int l = 0, r = arr.size() - 1;
+        int ans = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] <= time){
+                ans = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+    TimeMap_MemoryExceeded() {
+        mp.clear();    
+    }
+    
+    void set(string key, string value, int timestamp) {
+        string hash = key + "_" + to_string(timestamp);
+        
+        mp[hash] = value;
+        tmp[key].push_back(timestamp);
+    }
+    
+    string get(string key, int timestamp) {
+        
+        vector<int> timeStampArr = tmp[key];
+        
+        
+        int timeStampFinal = getTimeStamp(timestamp, timeStampArr);
+        
+        if(timeStampFinal == -1)
+            return "";
+        
+        string hash = key + "_" + to_string(timeStampArr[timeStampFinal]);
+        
+        return mp[hash];
+            
+        
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
+
+class TimeMap {
+public:
+    
+    map<string, vector<pair<int, string>>> mp;
+    
+    int getTimeStamp(int time, vector<int>& arr){
+        int l = 0, r = arr.size() - 1;
+        int ans = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] <= time){
+                ans = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    TimeMap() {
+
+    }
+    
+    void set(string key, string value, int timestamp) {
+        // mp[key][timestamp] = value;
+        
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        
+        // If key is not present in map
+        if(mp.find(key) == mp.end())
+            return "";
+         
+        // timestamp is less than timestamps present for the key
+        if(timestamp < mp[key][0].first)
+            return "";
+        
+        int l = 0;
+        int r = mp[key].size() - 1;
+        
+        string ans;
+        
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            
+            if(mp[key][mid].first == timestamp){
+                ans = mp[key][mid].second;
+                return ans;
+            } else if (mp[key][mid].first < timestamp){
+                ans = mp[key][mid].second;
+                l = mid+1;
+            }
+            else
+                r=mid-1;
+                
+        }
+        
+        return ans;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
