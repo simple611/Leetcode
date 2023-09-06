777b94f061d1fa907dc1b7b033172dfbe3e54a54
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-09-06 17:36:23 +0530

    Time: 20 ms (87.78%), Space: 89.2 MB (79.07%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // TC: O(m+n)  Sc - O(m+n)
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    double findMedianSortedArrays_mergeSort(vector<int>& nums1, vector<int>& nums2) {
         
         vector<int> nums3(nums1.size()+nums2.size());
         
@@ -38,6 +38,59 @@ public:
         }
         
         return ans;
+        
+    }
+    
+    // TC - O(m+n)  SC - O(1)
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        int idx1, idx2;
+        
+        idx2 = (m+n)/2;
+        idx1 = idx2-1;
+        
+        int count=0;
+        
+        int idx1el = -1, idx2el = -1;
+        int i=0, j=0;
+        
+        while(i<m && j<n){
+            
+            if(nums1[i] <= nums2[j]){
+                if(count == idx1)  idx1el=nums1[i];
+                if(count == idx2)  idx2el=nums1[i];
+                count++;
+                i++;
+            } else{
+                if(count == idx1)  idx1el=nums2[j];
+                if(count == idx2)  idx2el=nums2[j];
+                count++;
+                j++;
+            }
+        }
+        
+        while(i<m){
+            if(count == idx1)  idx1el=nums1[i];
+            if(count == idx2)  idx2el=nums1[i];
+            count++;
+            i++;
+        }
+        
+        while(j<n){
+            if(count == idx1)  idx1el=nums2[j];
+            if(count == idx2)  idx2el=nums2[j];
+            count++;
+            j++;
+        }
+        
+        if((m+n) %2 == 1){
+            return idx2el;
+        }
+        
+        return (double)((double)idx1el + idx2el)/2.0;
         
     }
 };
