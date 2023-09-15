666c7e68135711b87b4265e766369fc7dee9a4ec
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-09-15 16:43:08 +0530

    Time: 9 ms (87.69%), Space: 11.7 MB (8.11%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || k == 1)
+            return head;
+        
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode *cur = head;
+        
+        int count = 0;
+        while(cur){
+            count++;
+            cur = cur->next;
+        }
+        
+        int numOfGroups = count/k;
+        
+        ListNode *prev = NULL;
+        cur = head; 
+        
+        ListNode *start = dummy, *remaining;
+        for(int i=0;i<numOfGroups;i++){
+            prev = NULL;
+            remaining = cur;
+            
+            for(int i=0;i<k;i++){
+                ListNode *nex = cur->next;
+                cur->next = prev;
+                prev = cur;
+                cur = nex;
+            }
+            
+            start->next = prev;
+            remaining->next = cur;
+            start = remaining;
+            
+        }
+        
+        return dummy->next;
+    }
+};
