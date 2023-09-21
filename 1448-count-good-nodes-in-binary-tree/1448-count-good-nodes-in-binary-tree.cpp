e83e45ba2c2939603fae2a49bb6b559bcc38e5be
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-09-21 23:40:48 +0530

    Time: 102 ms (64.77%), Space: 86.6 MB (8.98%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int ans;
+    void dfs(TreeNode* root, int curMax){
+        if(root== NULL)
+            return;
+        
+        if(root->val >= curMax){
+            ans++;
+            curMax = root->val;
+        }
+        
+        dfs(root->left, curMax);
+        dfs(root->right, curMax);
+    }
+    int goodNodes(TreeNode* root) {
+        ans = 0;
+        dfs(root, root->val);
+        return ans;
+    }
+};
