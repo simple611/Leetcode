45a404de5200788b09d26aaa884311b9f34f8f5b
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-10-17 23:56:37 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (7.68%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    int maxRob(vector<int>& nums, int i, int start)
-    {
-        cout<<"i="<<i<<endl;
-        cout<<"start="<<start<<endl;
-        if(i<0 || i<start)
-            return 0;
-        if(i==0)
-            return nums[0];
-        if(i==1)
-            return max(nums[0], nums[1]);
-        if((i+1 % nums.size()) == start || i==start)
-        {
-            cout<<"int this"<<endl;
-            return maxRob(nums, i-1, start);
-        }
-        else
-        {
-             cout<<"int2 this"<<endl;
-            return max(nums[i] +maxRob(nums, i-2, start), maxRob(nums, i-1, start));
-        }
-    }
+//     int maxRob(vector<int>& nums, int i, int start)
+//     {
+//         cout<<"i="<<i<<endl;
+//         cout<<"start="<<start<<endl;
+//         if(i<0 || i<start)
+//             return 0;
+//         if(i==0)
+//             return nums[0];
+//         if(i==1)
+//             return max(nums[0], nums[1]);
+//         if((i+1 % nums.size()) == start || i==start)
+//         {
+//             cout<<"int this"<<endl;
+//             return maxRob(nums, i-1, start);
+//         }
+//         else
+//         {
+//              cout<<"int2 this"<<endl;
+//             return max(nums[i] +maxRob(nums, i-2, start), maxRob(nums, i-1, start));
+//         }
+//     }
     
-    // TC - O(N)  SC- O(N)
-    int rob_dp(vector<int>& nums) {
-        int n = nums.size()-1;
-        if(n<0)
-            return 0;
-        if(n==0)
-            return nums[0];
-        if(n==1)
-            return max(nums[0], nums[1]);
-        int start = (nums[0]> nums[1])? 0 :1;
-        return maxRob(nums, n, start);
-    }
+   
+//     int rob_dp(vector<int>& nums) {
+//         int n = nums.size()-1;
+//         if(n<0)
+//             return 0;
+//         if(n==0)
+//             return nums[0];
+//         if(n==1)
+//             return max(nums[0], nums[1]);
+//         int start = (nums[0]> nums[1])? 0 :1;
+//         return maxRob(nums, n, start);
+//     }
+    
     
     int houseRobber1(vector<int>& nums) {
         // dynamic programming - decide each problem by its sub-problems:
@@ -63,7 +64,8 @@ public:
             return max(prev_1, prev_2);
     }
     
-    // TC - O(N)  SC- O(N)
+    // TC - O(N)  SC- O(N+N) -> houseRobber1,
+               // SC - O(N) -> houseRobber
     int rob(vector<int>& nums) {
         // edge cases:
         if (nums.size() == 0) return 0;
