e1b5980d517f9f3f4d6fa01b0f5b8946be8bb3fa
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-10-17 23:15:37 +0530

    Time: 2 ms (47.43%), Space: 6.2 MB (55.06%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // TC = 2^n
+    int climbStairs_Recursive_TLE(int n) {
+        //Recursive approach ::TC = 2^n
+        if(n == 0 || n==1)
+            return 1;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+        
+    // Tc = O(n) ::SC = O(n)
+    int climbStairs(int n) {
+        // Bottom up approach with Memoization ::Tc = O(n) ::SC = O(n)
+        int num[n+1];
+        num[0]=1;
+        num[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            num[i]=num[i-1]+num[i-2];
+        }
+        return num[n];
+    }   
+        //Updated Bottom Up appraoch ::TC = O(n)  ::SC = 1
+        // if(n==0 || n==1)
+        //     return 1;
+        // int first = 1;
+        // int second = 1;
+        // int tmp = 0;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     tmp = first + second;
+        //     first = second;
+        //     second = tmp;
+        // }
+        // return second;
+    // }
+};
