c2396f80983f13c27ef7d3a9b7ac2cee7d396efe
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-10-17 23:29:04 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (46.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -23,11 +23,11 @@ public:
         // return maxRob(nums, nums.size()-1);
         
         // Recursion - Top down approach with memoization    
-        int n = nums.size();
-        int dp[n+1];
-        for(int i=0;i<=n;i++)
-            dp[i]=-1;
-        return maxRob(nums, n-1, dp);
+        // int n = nums.size();
+        // int dp[n+1];
+        // for(int i=0;i<=n;i++)
+        //     dp[i]=-1;
+        // return maxRob(nums, n-1, dp);
         
 //         //Iterative - Bottom up approach
 //         int n = nums.size();
@@ -46,20 +46,20 @@ public:
 //         return dp[n-1];
         
             //Iterative - Bottom up approach Modified
-//             int n = nums.size();
-//             if(n==0)
-//                 return 0;
-//             if(n==1)
-//                 return nums[0];
-//             int prev_2 = nums[0];
-//             int prev_1 = max(nums[0], nums[1]);
+            int n = nums.size();
+            if(n==0)
+                return 0;
+            if(n==1)
+                return nums[0];
+            int prev_2 = nums[0];
+            int prev_1 = max(nums[0], nums[1]);
 
-//             for(int i=2;i<n;i++)
-//             {
-//                 int tmp = max(nums[i]+prev_2, prev_1);
-//                 prev_2 = prev_1;
-//                 prev_1 = tmp;
-//             }
-//             return max(prev_1, prev_2);
+            for(int i=2;i<n;i++)
+            {
+                int tmp = max(nums[i]+prev_2, prev_1);
+                prev_2 = prev_1;
+                prev_1 = tmp;
+            }
+            return max(prev_1, prev_2);
     }
 };
