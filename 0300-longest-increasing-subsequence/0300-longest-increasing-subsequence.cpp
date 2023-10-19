9238e5a6b39f30406aace763cf661b916a1c3fd5
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2023-10-19 12:21:24 +0530

    Time: 3 ms (98.78%), Space: 10.8 MB (48.29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -7,6 +7,7 @@ public:
 
         for(int i=1;i<nums.size();i++){
             int maxI = 0;
+            // Find prev which are less than nums[i], and find value of lis which is max among them
             for(int j=0;j<i;j++){        
                 if(nums[i]>nums[j]){
                     maxI = max(maxI, lis[j]);
@@ -23,6 +24,11 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> res;
         for(int i=0; i<nums.size(); i++) {
+            // Check if the elememt is not in the res array, then add it
+            // else replace the element at lower bound with current element
+            // Example: [2, 5, 3, 4, 7]
+            // [2, 5]  => [2, 3]  , 5 replaced with 3 so that we can further have increasing subseq
+            // [2, 5, 7] => [2, 3, 4, 7]
             auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
             if(it==res.end()) res.push_back(nums[i]);
             else *it = nums[i];
